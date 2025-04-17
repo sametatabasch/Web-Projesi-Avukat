33e1ba3463106811fffdaa70333515c9be869368
sametatabasch <sametatabasch@gmail.com>
2025-04-17 10:55:33 +0300

    faaliyet alanları php ile düzenlendi

---
+++ sayfalar/faaliyet_alanlari.php
@@ -0,0 +1,230 @@
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area">
+        <div class="bradcam_inner bradcam_bg d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="bradcam_text text-center">
+                            <h3>Finest And Strongest Law <br>
+                                Firm Win The World</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end  -->
+    <!-- practice_area_start -->
+    <div class="practice_area practice_area2">
+        <div class="container-fluid p-0">
+            <div class="row no-gutters">
+                <div class="col-xl-3 col-md-6">
+                    <div class="single_practice">
+                        <div class="practice_image">
+                            <img src="img/practice/1.png" alt="">
+                        </div>
+                        <div class="practice_hover text-center">
+                            <div class="hover_inner">
+                                <i class="flaticon-case"></i>
+                                <h3>Business Law</h3>
+                                <p>There are many variations of passages of <br> Lorem Ipsum available, <br> but the
+                                    majority have suffered</p>
+                                <a href="#" class="lern_more">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="single_practice">
+                        <div class="practice_image">
+                            <img src="img/practice/2.png" alt="">
+                        </div>
+                        <div class="practice_hover text-center">
+                            <div class="hover_inner">
+                                <i class="flaticon-courthouse"></i>
+                                <h3>Finance Law</h3>
+                                <p>There are many variations of passages of <br> Lorem Ipsum available, <br> but the
+                                    majority have suffered</p>
+                                <a href="#" class="lern_more">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="single_practice">
+                        <div class="practice_image">
+                            <img src="img/practice/3.png" alt="">
+                        </div>
+                        <div class="practice_hover text-center">
+                            <div class="hover_inner">
+                                <i class="flaticon-judge"></i>
+                                <h3>Family Law</h3>
+                                <p>There are many variations of passages of <br> Lorem Ipsum available, <br> but the
+                                    majority have suffered</p>
+                                <a href="#" class="lern_more">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="single_practice">
+                        <div class="practice_image">
+                            <img src="img/practice/4.png" alt="">
+                        </div>
+                        <div class="practice_hover text-center">
+                            <div class="hover_inner">
+                                <i class="flaticon-jury"></i>
+                                <h3>Education Law</h3>
+                                <p>There are many variations of passages of <br> Lorem Ipsum available, <br> but the
+                                    majority have suffered</p>
+                                <a href="#" class="lern_more">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- practice_area_end -->
+    <!-- about_area _start  -->
+    <div class="about_area">
+        <div class="opacity_icon d-none d-lg-block">
+            <i class="flaticon-balance"></i>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-md-6">
+                    <div class="single_about_info text-center">
+                        <div class="about_thumb">
+                            <img src="img/about/1.png" alt="">
+                        </div>
+                        <h3>Finest And Strongest Law <br>
+                            Firm Win The World</h3>
+                        <p>There are many variations of passages of Lorem Ipsum <br> available, but the majority have
+                            suffered alteration in <br> some form, by injected humour, or randomised words <br> which
+                            don't look even slightly believable. </p>
+                        <div class="signature">
+                            <img src="img/about/signature.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="single_about_info text-center">
+                        <div class="about_thumb">
+                            <div class="image_hover">
+                                <div class="hover_inner">
+                                    <h2>93%</h2>
+                                    <span>Success Case</span>
+                                </div>
+                            </div>
+                        </div>
+                        <h3>About Lawyer Justice</h3>
+                        <p>There are many variations of passages of Lorem Ipsum <br> available, but the majority have
+                            suffered alteration in <br> some form, by injected humour, or randomised words <br> which
+                            don't look even slightly believable. </p>
+                        <div class="total_cases">
+                            <div class="single_cases">
+                                <h4>879</h4>
+                                <p>Total Cases</p>
+                            </div>
+                            <div class="single_cases">
+                                <h4>787</h4>
+                                <p>Case Won</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- about_area _end -->
+
+    <!-- testmonial_area_start  -->
+    <div class="testmonial_area testmonial_bg_1 overlay2">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="testmonial_active owl-carousel">
+                        <div class="single_testmonial text-center">
+                            <i class="flaticon-straight-quotes"></i>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the <br> majority have suffered alteration in some form, by injected humour, or <br> randomised words which don't look even slightly believable. </p>
+                            <div class="author_info d-flex justify-content-center align-items-center">
+                                <div class="thumb">
+                                    <img src="img/testmonial/smaill_thumb.png" alt="">
+                                </div>
+                                <span>- Millan Mirza</span>
+                            </div>
+                        </div>
+                        <div class="single_testmonial text-center">
+                            <i class="flaticon-straight-quotes"></i>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the <br> majority have suffered alteration in some form, by injected humour, or <br> randomised words which don't look even slightly believable. </p>
+                            <div class="author_info d-flex justify-content-center align-items-center">
+                                <div class="thumb">
+                                    <img src="img/testmonial/smaill_thumb.png" alt="">
+                                </div>
+                                <span>- Millan Mirza</span>
+                            </div>
+                        </div>
+                        <div class="single_testmonial text-center">
+                            <i class="flaticon-straight-quotes"></i>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the <br> majority have suffered alteration in some form, by injected humour, or <br> randomised words which don't look even slightly believable. </p>
+                            <div class="author_info d-flex justify-content-center align-items-center">
+                                <div class="thumb">
+                                    <img src="img/testmonial/smaill_thumb.png" alt="">
+                                </div>
+                                <span>- Millan Mirza</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- testmonial_area_end  -->
+
+    <div class="appointment_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-5 col-md-6 col-lg-6">
+                    <div class="appiontment_thumb d-none d-lg-block">
+                        <img src="img/appointment/1.png" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-6 offset-xl-1 col-md-6 col-md-12 col-lg-6">
+                    <div class="appointment_info">
+                        <div class="opacity_icon d-none d-lg-block">
+                            <i class="flaticon-balance"></i>
+                        </div>
+                        <h3>Make an Appointment</h3>
+                        <p>Many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some.</p>
+                        <form action="#">
+                                <div class="row">
+                                    <div class="col-xl-6 col-md-6">
+                                            <input type="text" placeholder="Your Name">
+                                    </div>
+                                    <div class="col-xl-6 col-md-6">
+                                            <input type="email" placeholder="Your Email">
+                                    </div>
+                                    <div class="col-xl-6 col-md-6">
+                                            <input type="text" placeholder="Phone no.">
+                                    </div>
+                                    <div class="col-xl-6 col-md-6">
+                                        <input id="datepicker" placeholder="Appointment date">
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <textarea placeholder="Message" ></textarea>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="appoinment_button">
+                                            <button class="boxed-btn5 " type="submit" >Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+        
+                            </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
